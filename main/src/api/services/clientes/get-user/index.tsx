616e668c92720102267cf8ee3a52679a72a1a06b
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-12-29 10:31:04 -0300

    Delete main/src/api/services directory

--- main/src/api/services/clientes/get-user/index.tsx
+++
@@ -1,7 +0,0 @@
-import axiosInstance from '@/api/axiosinstance';
-
-export const GetClient = async (): Promise<any> => {
-  const user = await axiosInstance.get<any>(`/cliente`);
-
-  return user.data;
-};
